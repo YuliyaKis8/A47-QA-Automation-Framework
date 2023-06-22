50323fa0ce763c3ef9ba8e688c45c63ca2fed200
YuliyaKis8 <yuliya.kiseleva@testpro.io>
2023-06-22 15:16:23 -0400

    hw16
    updated..

--- src/test/java/HomeWork16.java
+++ src/test/java/HomeWork16.java
@@ -15,20 +15,15 @@ public class HomeWork16 extends BaseTest {
     public void registrationNavigation(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*", "--incognito", "--start-full-screen");
-
         WebDriver driver = new ChromeDriver(options);
         String url = "https://qa.koel.app/";
-
         driver.get(url);
         WebElement registrationButton = driver.findElement(By.cssSelector("[id='hel']"));
         registrationButton.click();
-
         WebElement registerNewAccount = driver.findElement(By.xpath("//h2"));
         Assert.assertTrue(registerNewAccount.isDisplayed());
         driver.quit();
-
-
-
     }
-
 }
+
+
