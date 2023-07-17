8f5433ea076f073da23560fdccd51dc17de8b78f
YuliyaKis8 <yuliya.kiseleva@testpro.io>
2023-07-17 10:50:59 -0400

    HW24
    fixed

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -23,16 +23,17 @@ public class BaseTest {
     public static WebDriverWait wait = null;
     public static String url = "https://qa.koel.app/";
     public static Actions actions = null;
-//    @BeforeSuite
+
+    //    @BeforeSuite
 //    static void setupClass() {
 //        WebDriverManager.chromedriver().setup();
 //    }
     public static WebDriver pickBrowser(String browserName) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
         String gridURL = "http://192.168.1.151:4444";
-        switch(browserName){
+        switch (browserName) {
             case "firefox":
-            WebDriverManager.firefoxdriver().setup();
+                WebDriverManager.firefoxdriver().setup();
                 return driver = new FirefoxDriver();
             case "MSEdge":
                 WebDriverManager.edgedriver();
@@ -41,13 +42,13 @@ public class BaseTest {
                 return driver = new EdgeDriver(edgeOptions);
             case "grid-edge":
                 caps.setCapability("browserName", "MicrosoftEdge");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
             case "grid-firefox":
                 caps.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
             case "grid-chrome":
                 caps.setCapability("browserName", "chrome");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
             default:
                 WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
@@ -55,25 +56,36 @@ public class BaseTest {
                 return driver = new ChromeDriver(options);
         }
     }
+
     @BeforeMethod
     @Parameters({"BaseURL"})
     public void launchBrowser(String BaseURL) throws MalformedURLException {
         driver = pickBrowser((System.getProperty("browser")));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         actions = new Actions(driver);
-        wait = new WebDriverWait(driver,Duration.ofSeconds(5));
-        url =BaseURL;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        url = BaseURL;
         driver.get(url);
         navigateToPage();
     }
 
-    public  void navigateToPage() {
+    public void navigateToPage() {
         driver.get(url);
     }
+
     @AfterMethod
-    static void closeBrowser(){
+    static void closeBrowser() {
         driver.quit();
     }
 
+    @DataProvider(name = "IncorrectLoginProviders")
+    public static Object[][] getDataFromDataProviders() {
+        return new Object[][]{
+                {"NotExistingEmail@mail.com", "NotExistingPassword"},
+                {"yuliyakis85@gmail.com", " "},
+                {" ", " "},
+        };
+
     }
+}
 
